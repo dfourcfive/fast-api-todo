e4002d4d970c43ae7e748e0e239d569bd917bd8f
dfourcfive <i.benadidou@esi-sba.dz>
2022-11-12 19:12:42 +0100

    add login route

--- apis/v1/routes_user.py
+++ apis/v1/routes_user.py
@@ -1,13 +1,83 @@
-from db.repository.user_queries import create_new_user
-from fastapi import APIRouter
-from schemas.users import ShowUser
-from schemas.users import UserCreate
+from datetime import timedelta
+
+import jwt
+from jose import JWTError
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+
+from apis.utils import OAuth2PasswordBearerWithCookie
+from core.config import settings
+from core.hashing import Hasher
+from core.security import create_access_token
+from db.repository.user_queries import create_new_user, get_user_by_username , get_user_by_id
+from db.session import get_db
+from schemas.token import Token
+from schemas.users import ShowUser, UserCreate
 
 router = APIRouter()
 
 
-@router.post("/", response_model=ShowUser)
+@router.post("/register", response_model=ShowUser)
 def create_user(user: UserCreate):
     user = create_user(user=user)
+    return user
+
+
+@router.post("/login", response_model=ShowUser)
+def create_user(user: UserCreate):
+    user = create_user(user=user)
+    return user
+
+
+
+
+@router.post("/token", response_model=Token)
+def login_for_access_token(
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"id": user.id}, expires_delta=access_token_expires
+    )
+    response.set_cookie(
+        key="access_token", value=f"Bearer {access_token}", httponly=True
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+def get_current_user_from_token(
+    token: str
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        id: str = payload.get("id")
+        print("id extracted is ", id)
+        if id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user_by_id(id=id)
+    if user is None:
+        raise credentials_exception
+    return 
+
+def authenticate_user(username: str, password: str):
+    user = get_user_by_username(username=username)
+
+    if not user:
+        return False
+    if not Hasher.verify_password(password, user.hashed_password):
+        return False
     return user
--- db/repository/user_queries.py
+++ db/repository/user_queries.py
@@ -9,8 +9,13 @@ def get_user_by_id(id: str):
     user = db.query(User).filter(User.id == id).first()
     return user
 
+def get_user_by_username(username: str):
+    user = db.query(User).filter(User.username == username).first()
+    return user
 
 def create_user(user: User):
+    if (is_username_unique(user)&is_email_unique(user)):
+        return False
     db.add(user)
     db.commit()
     return user
@@ -19,3 +24,14 @@ def update_user(user: User):
     db.update(user)
     db.commit()
     return user
+
+
+def is_username_unique(user: User):
+    if len(db.query(User).filter(User.username != user.username)) == 0:
+        return True
+    return False
+
+def is_email_unique(user: User):
+    if len(db.query(User).filter(User.email != user.email)) == 0:
+        return True
+    return False
