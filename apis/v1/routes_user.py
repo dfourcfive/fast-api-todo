5ce3d41cbdedb5568f8452e9cfcc7ac713ca10aa
dfourcfive <i.benadidou@esi-sba.dz>
2022-11-13 16:18:20 +0100

    fix session connection , and update quries

--- apis/v1/routes_user.py
+++ apis/v1/routes_user.py
@@ -10,7 +10,7 @@ from apis.utils import OAuth2PasswordBearerWithCookie
 from core.config import settings
 from core.hashing import Hasher
 from core.security import create_access_token
-from db.repository.user_queries import get_user_by_username , get_user_by_id
+from db.repository.user_queries import get_user_by_username , get_user_by_id , create_user
 from db.session import get_db
 from schemas.token import Token
 from schemas.users import ShowUser, UserCreate
@@ -19,23 +19,29 @@ user_router = APIRouter()
 
 
 @user_router.post("/register", response_model=ShowUser)
-def create_user(user: UserCreate):
+def create_user_route_method(user: UserCreate,db: Session = Depends(get_db)):
     try:
       updated_user = user
-      hashed_password = Hasher.get_password_hash(user.password)
+      hashed_password = Hasher.get_password_hash(updated_user.password)
       updated_user.password=hashed_password
-      create_user(updated_user)
-      return updated_user
+      print(updated_user.password)
+      result = create_user(updated_user,db=db)
+      if(result == False):
+            raise HTTPException(status_code=404, detail="username or email is not unique")
+      print('here no errors so far')
+      response=ShowUser(username=result.username,email=result.email,jwt="")
+      return response
     except Exception as e:
       print('An exception occurred'+str(e))
-
+      raise HTTPException(status_code=404, detail="an error occured")
 
 
 @user_router.post("/token", response_model=Token)
 def login_for_access_token(
     self,
     response: Response,
-    form_data: OAuth2PasswordRequestForm = Depends()):
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
@@ -54,8 +60,8 @@ def login_for_access_token(
 
 
 
-def authenticate_user(username: str, password: str):
-    user = get_user_by_username(username=username)
+def authenticate_user(username: str, password: str,db: Session = Depends(get_db)):
+    user = get_user_by_username(username=username,db=db)
 
     if not user:
         return False
--- db/repository/user_queries.py
+++ db/repository/user_queries.py
@@ -1,39 +1,43 @@
 from db.models.user import User
 from sqlalchemy.orm import Session
-from db.session import get_db
+from db.session import SessionLocal , get_db
 from schemas.users import UserCreate
 
 
 
-db = get_db
-def get_user_by_id(id: str):
+def get_user_by_id(id: str,db:Session):
     user = db.query(User).filter(User.id == id).first()
     return user
 
-def get_user_by_username(username: str):
+def get_user_by_username(username: str,db:Session):
     user = db.query(User).filter(User.username == username).first()
     return user
 
-def create_user(user_create: UserCreate):
-    user_object = User(**user_create.dict())
-    if (is_username_unique(user_object)&is_email_unique(user_object)):
+def create_user(user_create: UserCreate,db:Session):
+    user_object = User(
+        username=user_create.username,
+        email=user_create.email,
+        hashed_password=user_create.password,
+        is_email_confirmed=False
+    )
+    if (is_username_unique(user_object,db=db)&is_email_unique(user_object,db=db)):
         return False
     db.add(user_object)
     db.commit()
     return user_create
 
-def update_user_by_user(user: User):
-    db.update(user)
+def update_user_by_user(user: User,db:Session):
+    db.refresh(user)
     db.commit()
     return user
 
 
-def is_username_unique(user: User):
-    if len(db.query(User).filter(User.username != user.username)) == 0:
+def is_username_unique(user: User,db:Session):
+    if (db.query(User).filter(User.username != user.username).all.size == 0):
         return True
     return False
 
-def is_email_unique(user: User):
-    if len(db.query(User).filter(User.email != user.email)) == 0:
+def is_email_unique(user: User,db:Session):
+    if (db.query(User).filter(User.email != user.email).all.size == 0):
         return True
     return False
